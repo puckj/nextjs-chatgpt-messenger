d84684d9fb2bcc0f8f900c3899bc1d3cd59aca78
Pongsapuck_Dev <pongsapuck.j@gmail.com>
2023-06-02 09:56:57 +0700

    setup next-auth & google sign-in

---
+++ src/components/Login.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+
+function Login() {
+  return (
+    <div
+      className="bg-[#11A37F] h-screen flex flex-col items-center
+    justify-center text-center"
+    >
+      <Image
+        src={require("../../public/images/ChatGPT-logo.png")}
+        width={300}
+        height={300}
+        alt="logos"
+      />
+      <button
+        onClick={() => signIn("google")}
+        className="text-white font-bold text-3xl animate-pulse"
+      >
+        Sign In to use ChatGPT
+      </button>
+    </div>
+  );
+}
+
+export default Login;
