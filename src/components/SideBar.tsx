66831451a209255f9804fc5f6ac8a3b47feb63ff
Pongsapuck_Dev <pongsapuck.j@gmail.com>
2023-06-02 11:32:50 +0700

    signIn & signOut functions

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,17 +1,30 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { signOut, useSession } from "next-auth/react";
 import NewChat from "./NewChat";
 
 function SideBar() {
+  const { data: session } = useSession();
+
   return (
     <div className="p-2 flex flex-col h-screen">
       <div className="flex-1">
         <div>
-          <NewChat/>
-          <div>
-            {/* ModelSelection */}
-          </div>
+          <NewChat />
+          <div>{/* ModelSelection */}</div>
           {/* Map through the ChatRows  */}
         </div>
       </div>
+      {session && (
+        <img
+          onClick={() => signOut()}
+          src={session.user?.image!}
+          alt="profile pic"
+          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2
+          hover:opacity-50"
+        /> /*ไม่ใช้ Image Components จาก next เพราะว่าบางครั้ง nextjs จะไม่ยอมรับ url(pic url) ที่มาจาก external links */
+      )}
     </div>
   );
 }
