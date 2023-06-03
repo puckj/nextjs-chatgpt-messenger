58fe77e4c3a23f827326356072bf03f72b5074e8
puckj <pongsapuck.j@gmail.com>
2023-06-03 10:57:50 +0700

    nextjs.config.js for use env (client-side)

--- firebase.ts
+++ firebase.ts
@@ -6,10 +6,10 @@ const firebaseConfig = {
   apiKey: process.env.FB_API_KEY,
   authDomain: process.env.FB_AUTH_DOMAIN,
   projectId: process.env.FB_PROJECT_ID,
-  storageBucket: process.env.FB_STORAGE_BUCKEY,
+  storageBucket: process.env.FB_STORAGE_BUCKET,
   messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
   appId: process.env.FB_APP_ID,
-};
+}; //ถ้าอ้างอิง process.env โดยตรงจาก env.local ฝั่ง client-side (หน้า browser) จะมองไม่เห็น (undefined) ต้องไป config ใน next.config.js ก่อน
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
--- next.config.js
+++ next.config.js
@@ -2,8 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    appDir: true
-  }
-}
+    appDir: true,
+  },
+  env: {
+    FB_API_KEY: process.env.FB_API_KEY,
+    FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN,
+    FB_PROJECT_ID: process.env.FB_PROJECT_ID,
+    FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKEY,
+    FB_MESSAGING_SENDER_ID: process.env.FB_MESSAGING_SENDER_ID,
+    FB_APP_ID: process.env.FB_APP_ID,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
